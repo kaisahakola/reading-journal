387a40e732de6c2368414cd0045b06343207c7b7
Kaisa Hakola <kaisahakola00@gmail.com>
2025-10-22 12:06:37 +0300

    Update date.ts
    Update the parseDate function so that months with double digits are visible correctly.

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,6 @@
 export const parseDate = (date: string) => {
   const year = date.slice(0, 4);
-  const month = date.slice(6, 7);
+  const month = date.slice(5, 7);
   const day = date.slice(8, 10);
 
   return `${day}.${month}.${year}`;
