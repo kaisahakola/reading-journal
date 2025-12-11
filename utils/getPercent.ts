1dd5ee0eb4cd42a94854a7575ca027939041d6ab
Kaisa Hakola <kaisahakola00@gmail.com>
2025-12-11 19:10:06 +0200

    Remove console logs from getPercent

--- utils/getPercent.ts
+++ utils/getPercent.ts
@@ -1,8 +1,4 @@
 export const getPercent = (count: number, all: number) => {
   const percent: number = (count / all) * 100;
-  console.log('count: ', count);
-  console.log('all: ', all);
-  console.log('count / all: ', count / all);
-  console.log('count / all * 100: ', (count / all) * 100);
   return `${percent.toFixed(0)}%`;
 };
